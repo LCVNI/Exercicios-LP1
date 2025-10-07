824a47e58ff8e49f26a6a76d41a6e76c167cb8a9
Lucas <153649430+LCVNI@users.noreply.github.com>
2025-10-07 10:21:47 -0300

    Update espelhado.c

--- espelhado.c
+++ espelhado.c
@@ -1,24 +1,46 @@
 /*Leia um número inteiro n e gere uma sequência
 “espelhada” dos n primeiros números primos.*/
+#include<stdio.h>
+#include<stdlib.h>
 void espelhar(int n){
-    int cont=0, primos=0;
-    while(primos <= n){
+    int cont=0, primos=0, i, j,k;
 
-        if(primos <= n/2){
-            for(int i=1; primos <= n; i++){
-
-                for(int j=1; j <= i; j++){
-                    if(j % i == 0){
-                    cont++;
-                    }
-                }
-            if(cont == 2){
-                printf("%d", i);
-                primos++;
-            }
-            cont=0;
+    for(i=1; primos < n; i++){
+        for(j=1; j <= i; j++){
+            if(i % j == 0){
+                cont++;
             }
         }
+        if(cont == 2){
+            printf("%d", i);
+            primos++;
+        }
+        cont=0;
+    }
+    printf(" / ");
+    // Espelhado
+    for(; i > 1; i--){
+        for(k=1; k <= i; k++){
+            if(i % k == 0){
+                cont++;
+            }
+        }
+        if(cont == 2){
+            printf("%d", i);
+        }
+        cont=0;
     }
 }
-//Se i>=n,  
+
+int main(){
+    int n;
+    printf("Informe um numero: ");
+    scanf("%d", &n);
+    printf("\n");
+    espelhar(n);
+    printf("\n");
+    system ("Pause");
+    return 0;
+}
+    
+  
